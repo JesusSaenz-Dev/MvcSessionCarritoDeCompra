3a7fb1a8d7aa7749df830a98d7fd0c38cedada1e
JesusSaenz-Dev <61250238+JesusSaenz-Dev@users.noreply.github.com>
2020-04-21 18:37:02 +0200

    Add files via upload

---
+++ CarritoCompraSession/Data/ContextoProducto.cs
@@ -0,0 +1,15 @@
+﻿using CarritoCompraSession.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CarritoCompraSession.Data
+{
+    public class ContextoProducto : DbContext
+    {
+        public ContextoProducto() : base("conexionproductos") { }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
---
+++ CarritoCompraSession/Models/Producto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarritoCompraSession.Models
+{
+    [Table("Productos")]
+    public class Producto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column("IdProducto")]
+        public int IdProducto { get; set; }
+
+        [Column("Nombre")]
+        public String Nombre  { get; set; }
+
+        [Column("Precio")]
+        public int Precio { get; set; }
+
+        [Column("Imagen")]
+        public String Imagen  { get; set; }
+    }
+}
